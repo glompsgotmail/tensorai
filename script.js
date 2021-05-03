f1143d8e7e61381577f8551852400f5687b5472c
glompsgotmail <44075137+glompsgotmail@users.noreply.github.com>
2021-05-03 12:15:58 -0400

    Added custom input support for linear functions
    I noticed the limitation of the AI is that it only is effective for linear functions. I think it might have something to do with the layers and amount of neurons but I have no fucking clue how to remedy this

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
+//training function
 async function gaber(){
+    //button disabling when training
     const button = document.querySelector('button')
     button.disabled = true;
+    //get and parse training data from input forms
+    let xarray = document.getElementById("xvalues").value.replace(/\s/g, '').split`,`.map(x=>+x);
+    let yarray = document.getElementById("yvalues").value.replace(/\s/g, '').split`,`.map(x=>+x);
+    if(xarray.length!=yarray.length){
+        document.getElementById('out').innerText = "Data values don't match up!";
+        return
+    }
+    //get the amount of epochs from form and turn it into an int
     let isepocs = document.getElementById("epochs").value;
     let domestic = parseInt(isepocs);
+    //create the AI
     const sumk = tf.sequential();
     sumk.add(tf.layers.dense({units: 1, inputShape:[1]}));
     sumk.compile({
         loss: 'meanSquaredError',
         optimizer: 'sgd'
     });
-    const xs = tf.tensor2d([-1,0,1,2,3,4],[6,1]);
-    const ys = tf.tensor2d([-3,-1,1,3,5,7],[6,1]);
+    //convert data to tensors
+    const xs = tf.tensor2d(xarray,[xarray.length,1]);
+    const ys = tf.tensor2d(yarray,[yarray.length,1]);
+    //train AI
     await sumk.fit(xs,ys, {epochs:domestic});
+    //save AI params to local storage
     await sumk.save('localstorage://mothel');
+    // re-enable button when done training
     button.disabled = false;
+    //output text when done training
     document.getElementById('out').innerText = "Done Training!";
 }
-
+//prediction function
 async function thredict(){
+    //get input from form and turn it into an int
     let input = document.getElementById("in").value;
     let intl = parseInt(input);
+    //load model from local storage
     const sumk = await tf.loadLayersModel('localstorage://mothel');
+    //process and output the prediction
     document.getElementById('out').innerText = sumk.predict(tf.tensor2d([intl],[1,1])).arraySync();
 
 }
