e5a6fbf5778757cc41053a9f0e55927bc3151963
glompsgotmail <44075137+glompsgotmail@users.noreply.github.com>
2021-05-10 08:34:05 -0400

    added quadratics
    accelerator from a certain magical index is best boy

--- script.js
+++ script.js
@@ -1,45 +1,114 @@
-//training function
-async function gaber(){
-    //button disabling when training
-    const button = document.querySelector('button')
-    button.disabled = true;
-    //get and parse training data from input forms
-    let xarray = document.getElementById("xvalues").value.replace(/\s/g, '').split`,`.map(x=>+x);
-    let yarray = document.getElementById("yvalues").value.replace(/\s/g, '').split`,`.map(x=>+x);
+
+
+/**
+ * We want to learn the coefficients that give correct solutions to the
+ * following cubic equation:
+ *      y = a * x^3 + b * x^2 + c * x + d
+ * In other words we want to learn values for:
+ *      a
+ *      b
+ *      c
+ *      d
+ * Such that this function produces 'desired outputs' for y when provided
+ * with x. We will provide some examples of 'xs' and 'ys' to allow this model
+ * to learn what we mean by desired outputs and then use it to produce new
+ * values of y that fit the curve implied by our example.
+ */
+
+// Step 1. Set up variables, these are the things we want the model
+// to learn in order to do prediction accurately. We will initialize
+// them with random values.
+let a = tf.variable(tf.scalar(Math.random()));
+let b = tf.variable(tf.scalar(Math.random()));
+let c = tf.variable(tf.scalar(Math.random()));
+let d = tf.variable(tf.scalar(Math.random()));
+
+
+// Step 2. Create an optimizer, we will use this later. You can play
+// with some of these values to see how the model performs.
+const learningRate = 0.0001;
+const optimizer = tf.train.sgd(learningRate);
+
+// Step 3. Write our training process functions.
+
+/*
+ * This function represents our 'model'. Given an input 'x' it will try and
+ * predict the appropriate output 'y'.
+ *
+ * It is also sometimes referred to as the 'forward' step of our training
+ * process. Though we will use the same function for predictions later.
+ *
+ * @return number predicted y value
+ */
+function predict(x) {
+  // y = a * x ^ 3 + b * x ^ 2 + c * x + d
+  return tf.tidy(() => {
+    return a.mul(x.pow(tf.scalar(3, 'int32')))
+      .add(b.mul(x.square()))
+      .add(c.mul(x))
+      .add(d);
+  });
+}
+
+/*
+ * This will tell us how good the 'prediction' is given what we actually
+ * expected.
+ *
+ * prediction is a tensor with our predicted y values.
+ * labels is a tensor with the y values the model should have predicted.
+ */
+function loss(prediction, labels) {
+  // Having a good error function is key for training a machine learning model
+  const error = prediction.sub(labels).square().mean();
+  return error;
+}
+
+/*
+ * This will iteratively train our model.
+ *
+ * xs - training data x values
+ * ys — training data y values
+ */
+async function train(xs, ys, numIterations) {
+  for (let iter = 0; iter < numIterations; iter++) {
+    // optimizer.minimize is where the training happens.
+
+    // The function it takes must return a numerical estimate (i.e. loss)
+    // of how well we are doing using the current state of
+    // the variables we created at the start.
+
+    // This optimizer does the 'backward' step of our training process
+    // updating variables defined previously in order to minimize the
+    // loss.
+    optimizer.minimize(() => {
+      // Feed the examples into the model
+      for(let i = 0; i<xs.length;i++){
+      const pred = predict(xs[i]);
+      return loss(pred, ys[i]);
+    }
+    });
+
+    // Use tf.nextFrame to not block the browser.
+    await tf.nextFrame();
+  }
+}
+
+async function learn() {
+    const btn = document.querySelector("button");
+    btn.disabled=true;
+    let xarray = document.getElementById("xvalues").value.replace(/\s/g, '').split`,`.map(x=>+x).map(x=>tf.tensor(x,[1,1]));
+    let yarray = document.getElementById("yvalues").value.replace(/\s/g, '').split`,`.map(x=>+x).map(x=>tf.tensor(x,[1,1]));
     if(xarray.length!=yarray.length){
         document.getElementById('out').innerText = "Data values don't match up!";
         return
     }
-    //get the amount of epochs from form and turn it into an int
     let isepocs = document.getElementById("epochs").value;
     let domestic = parseInt(isepocs);
-    //create the AI
-    const sumk = tf.sequential();
-    sumk.add(tf.layers.dense({units: 1, inputShape:[1]}));
-    sumk.compile({
-        loss: 'meanSquaredError',
-        optimizer: 'sgd'
-    });
-    //convert data to tensors
-    const xs = tf.tensor2d(xarray,[xarray.length,1]);
-    const ys = tf.tensor2d(yarray,[yarray.length,1]);
-    //train AI
-    await sumk.fit(xs,ys, {epochs:domestic});
-    //save AI params to local storage
-    await sumk.save('localstorage://mothel');
-    // re-enable button when done training
-    button.disabled = false;
-    //output text when done training
-    document.getElementById('out').innerText = "Done Training!";
+  // Train the model!
+    await train(xarray, yarray, domestic);
+    btn.disabled=false;
 }
-//prediction function
-async function thredict(){
-    //get input from form and turn it into an int
-    let input = document.getElementById("in").value;
-    let intl = parseInt(input);
-    //load model from local storage
-    const sumk = await tf.loadLayersModel('localstorage://mothel');
-    //process and output the prediction
-    document.getElementById('out').innerText = sumk.predict(tf.tensor2d([intl],[1,1])).arraySync();
 
+async function thredict(){
+    document.getElementById("out").innerText = predict(tf.tensor(parseInt(document.getElementById("in").value))).dataSync();
 }
